64874df87f34c4e8edb883b7662e6754f7192b14
Oleg <beezlerey55@gmail.com>
2023-10-12 20:11:36 +0300

    Исправление

--- js/functions.js
+++ js/functions.js
@@ -1,54 +1,41 @@
 //Функция для проверки длины строки.
-function getLineWidth(value,num) {
-  // if (value.length <= num) {
-  //   return true;
-  // }
-  // return false;
-  //const lineWidth = value.length <= num ? true : false;
-  const result = value.length <= num;
-  return result;
+function checkLengthString(value,num) {
+  return value.length <= num;
 }
-getLineWidth('проверяемая строка', 20); // true
-getLineWidth('проверяемая строка', 18); // true
-getLineWidth('проверяемая строка', 10); // false
+checkLengthString('проверяемая строка', 20); // true
+checkLengthString('проверяемая строка', 18); // true
+checkLengthString('проверяемая строка', 10); // false
 
 //Функция для проверки, является ли строка палиндромом.
 function getPalindrome(value) {
+  value = value.toLowerCase();
   let expandedValue = '';
   let newValue = '';
   // reversed string, without spaces, lower
   for (let i = value.length - 1; i >= 0; i--) {
     if (value[i] !== ' ') {
-      expandedValue += value[i].toLowerCase();
+      expandedValue += value[i];
     }
   }
   // normal string, without spaces, lower
-  for (let i = 0; i < value.length; i++) {
-    if (value[i] !== ' ') {
-      newValue += value[i].toLowerCase();
-    }
-  }
-  const result = expandedValue === newValue;
-  return result;
+  newValue = value.replaceAll(' ', '');
+  return expandedValue === newValue;
 }
 getPalindrome('топот'); // true
 getPalindrome('ДовОд'); // true
 getPalindrome('Кекс'); // false
 getPalindrome('Лёша на полке клопа нашёл '); // true
 
-
 /* Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 */
-
 function getNum(value) {
   value = String(value);
   let result = '';
   for (let i = 0; i < value.length; i++) {
-    if (Number(value[i]) || value[i] === '0') {
+    if (!isNaN(parseInt(value[i]))) {
       result += value[i];
     }
   }
-  result = parseInt(result, NaN);
-  return result;
+  return parseInt(result);
 }
 getNum('2023 год'); // 2023
 getNum('ECMAScript 2022'); // 2022
