6a7503cb41210c3a173d17132c4f20718acbf04b
Oleg <beezlerey55@gmail.com>
2023-10-19 11:11:36 +0300

    delete arrayUrl; delete function getUrl

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const COMMENTS_NAME = ['Артём', 'Олег', 'Антон', 'Вадим', 'С
 const DESCRIPTION = ['Великолепные пейзажи', 'Фото на память', 'Mood', 'Без слов', 'Как пить дать', 'Во всей красе'];
 let commentsIdIndicate = 1;
 const COMMENTS_MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',];
-const ARRAY_URL = [];
 const ARRAY_ID = [];
 function getRandomNumber (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -15,7 +14,7 @@ function createObject() {
   const result = [];
   const obj = {
     id: createObjId(1, 25),
-    url: createObjUrl(1, 25),
+    url: `photos/${getRandomNumber(1, 25)}.jpg`,
     description: DESCRIPTION[getRandomNumber(1,6)],
     likes: getRandomNumber(15, 200),
     comments: createObjComments(0, 30),
@@ -32,16 +31,6 @@ function createObjId(min, max) {
   ARRAY_ID.push(num);
   return num;
 }
-
-function createObjUrl(min, max) {
-  let num = getRandomNumber(min, max);
-  while (ARRAY_URL.includes(num)){
-    num = getRandomNumber(1, 25);
-  }
-  ARRAY_URL.push(num);
-  return `photos/${num}.jpg`;
-}
-
 function createObjComments(min, max) {
   const arrayComments = [];
 
